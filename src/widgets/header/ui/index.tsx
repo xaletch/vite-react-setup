b31d6f48e754480aec8f88d64445a03f910a7735
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-06-13 23:28:20 +0300

    bug fixed

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -1,17 +1,7 @@
-import { Link } from "react-router-dom"
-
 export const Header = () => {
   return (
     <div>
       header
-      <ul>
-        <li>
-          <Link to={'/'}>Home page</Link>
-        </li>
-        <li>
-          <Link to={'/profile'}>Profile page</Link>
-        </li>
-      </ul>
     </div>
   )
 }
