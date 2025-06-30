b3918f16a78e1fa8fa7a1c0e01fc0e009bd21cef
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-07-01 00:17:06 +0300

    ui: card

---
+++ src/shared/ui/card/card.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/shared/lib";
+import React from "react";
+
+export type ReactDiveAttributer = React.HTMLAttributes<HTMLElement>;
+
+const Card = React.forwardRef<HTMLDivElement, ReactDiveAttributer>(({ className, ...props }, ref) => (
+  <div 
+    ref={ref}
+    className={cn("bg-gray-200 rounded-3xl px-6 py-4", className)}
+    {...props}
+  />
+));
+Card.displayName = "Card";
+
+export { Card };
