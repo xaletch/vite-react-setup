fc6375d654de907ac3267aafc5c26b529834e979
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-07-02 00:32:02 +0300

    close storybook test

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,10 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+// import { fileURLToPath } from 'node:url';
+// import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+// const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [tanstackRouter({
     target: 'react',
@@ -22,27 +21,29 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
-        },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+
+
+  // test: {
+  //   projects: [{
+  //     extends: true,
+  //     plugins: [
+  //     // The plugin will run tests for the stories defined in your Storybook config
+  //     // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+  //     storybookTest({
+  //       configDir: path.join(dirname, '.storybook')
+  //     })],
+  //     test: {
+  //       name: 'storybook',
+  //       browser: {
+  //         enabled: true,
+  //         headless: true,
+  //         provider: 'playwright',
+  //         instances: [{
+  //           browser: 'chromium'
+  //         }]
+  //       },
+  //       setupFiles: ['.storybook/vitest.setup.ts']
+  //     }
+  //   }]
+  // }
 });
